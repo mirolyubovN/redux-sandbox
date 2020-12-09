4da3980140fc924c431ae9722d201ec6b38c4aa9
nikita.mirolyubov <mirolyubov_n@yahoo.co.uk>
2020-12-09 16:59:11 +0500

    refactoring, bugfixes : added margin to buttons, removed duplicate div

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Counter from "./counter";
+import './app.css';
 
 const App = () => {
     return <Counter />;
--- src/components/counter.js
+++ src/components/counter.js
@@ -5,7 +5,7 @@ import * as actions from '../actions';
 
 const Counter = ({ counter, inc, dec, rnd }) => {
     return (
-        <div id="root" className="jumbotron">
+        <>
             <h2 id="counter">{counter}</h2>
             <button
                 onClick={dec}
@@ -17,7 +17,7 @@ const Counter = ({ counter, inc, dec, rnd }) => {
             <button onClick={rnd}
                     id="rnd"
                     className="btn btn-primary btn-lg">RND</button>
-        </div>
+        </>
     );
 };
 const mapStateToProps = (state) => {
